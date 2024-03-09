dbed87564df619c1b9856eec37de8b6bb0cf14bf
Dr. Eerie <96463088+DrEerie@users.noreply.github.com>
2024-03-09 07:56:09 +0500

    Delete Cpp-CodeFiles directory

--- Cpp-CodeFiles/C10practicalNo1Alternative.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main( void )
-{
-cout << "\n\n\t\t  WELCOME TO OUR SCHOOL" << endl;
-cout << " \t\t  Welcome to De Technocrats" << endl ;
-return 0;
-}
